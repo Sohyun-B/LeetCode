fcfce45d2526e575b43c53f23a70728e3d119665
Sohyun-B <90600397+Sohyun-B@users.noreply.github.com>
2024-08-09 18:31:36 +0900

    Time: 280 ms (26.97%), Space: 25.7 MB (47.29%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,5 +1,17 @@
 from typing import List
+from collections import Counter
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        sum = eval('*'.join([str(n) for n in nums]))
-        return [sum//n for n in nums]
+        out = []
+        p = 1
+        # 왼쪽 곱셈
+        for i in range (len(nums)):
+            out.append(p)
+            p = p * nums[i]
+
+        p = 1 # 값 초기화
+        # 오른쪽 곱셈
+        for i in range(len(nums)-1, 0 -1, -1):
+            out[i] = out[i] * p
+            p = p * nums[i]
+        return out
