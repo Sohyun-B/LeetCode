c13cc854ef55e064a8efffe1115fd6d659e87142
Sohyun-B <90600397+Sohyun-B@users.noreply.github.com>
2024-08-20 12:48:32 +0900

    Time: 11 ms (82.05%), Space: 11.5 MB (86.66%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
@@ -1,7 +1,15 @@
 class Solution(object):
     def swapPairs(self, head):
-        root = head
+        root = prev = ListNode(0)
+        prev.next = head
         while head and head.next:
-            head.val, head.next.val = head.next.val, head.val
-            head = head.next.next
-        return root
+            temp = head.next 
+            head.next = temp.next # temp.next가 없을 경우 none을 반환한다
+            temp.next = head
+
+            prev.next = temp
+
+            # 다음으로 이동
+            head = head.next 
+            prev = prev.next.next
+        return root.next
