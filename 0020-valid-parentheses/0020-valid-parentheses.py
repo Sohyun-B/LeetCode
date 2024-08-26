0e2604ad77ce56cb85df1f66a00b81aa903e540a
Sohyun-B <90600397+Sohyun-B@users.noreply.github.com>
2024-08-26 13:42:54 +0900

    Time: 22 ms (10.66%), Space: 11.7 MB (81.47%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,22 +1,14 @@
 class Solution(object):
     def isValid(self, s):
         stack =[]
-        for i in list(s):
-            if i =="(":
-                stack.append(")")
-            elif i == "[":
-                stack.append("]")
-            elif i == "{":
-                stack.append("}")
-            else: # 닫는 괄호가 나오는 경우
-                # 이미 스택이 존재하고 그 스택에 해당하는 괄호가 있어야한다
-                if stack and stack.pop() == i:
-                    continue
-                # 없으면 False
+        table = {
+            ')':"(",
+            "}":"{",
+            "]":"["
+		}
+        for char in s:
+            if char not in table:
+                stack.append(char)
+            elif not stack or table[char] !=stack.pop():
                 return False
-        # 스택에 남아있는 경우: 
-		# = 여는 괄호가 닫는 괄호보다 많은 경우
-		# False 리턴
-        if stack:
-            return False
-        return True
+        return len(stack)==0
